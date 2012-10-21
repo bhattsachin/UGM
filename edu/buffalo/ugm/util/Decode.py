9f26977710bd77431f7e7f62c0e20d129ec4068c
Sachin Bhatt <bhattsachin@gmail.com>
2012-10-21 00:20:58 -0400

    recursive block

--- edu/buffalo/ugm/util/Decode.py
+++ edu/buffalo/ugm/util/Decode.py
@@ -38,24 +38,30 @@ class Decode(object):
         
         #lets apply permutation to first n(nodes) columns and list all possible values
         #assuming states will be numbers starting from zero to stated value. FIXME: we should not live on these assumptions
-        self.enumerateAll(0, 3, [3], 1)
+        self.enumerateAll(0, len(edgeStruct.nodes) - 1, [3], edgeStruct.nStates)
         
         print str(self.table)
-        
+      
+    '''
+    Recursive function to enlist all set of possibilities for given input
+    @param cNode: current node (starts with zero), assuming they are in increasing order
+    @param tNode: total nodes, enumeration starts with cNode and runs till it has been incremented to tNode
+    @param vArray: records all the assignments during that backtrack from root to leaf node
+    @param tState: total number of states. Again it starts with zero and runs till currState<tState. incrementing by one each time   
+    '''    
     def enumerateAll(self, cNode, tNode, vArray, tState):
-        
         for i in range(tState):
-            print "cNode:" + str(cNode) + " i:" + str(i)
+            #print "cNode:" + str(cNode) + " i:" + str(i)
             if cNode == 0: #if this is first node - reinitialize the array - not so necessary though
-                vArray = []
-            vArray.append(i)
+                vArray = [0 for x in range(tNode + 1)]
+            vArray[cNode]=i
             if cNode < tNode: #if we are not at the last dude, keep diving inside
                 self.enumerateAll(cNode+1,tNode, vArray, tState)
             else:
-                for j in range(tNode):
+                for j in range(tNode + 1):
                     self.table[self.cRow][j] = vArray[j]
                 self.cRow = self.cRow+1
-                #return
+                
         
             
         
